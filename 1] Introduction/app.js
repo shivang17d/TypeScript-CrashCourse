7b72acb7943411aeb157ea9c794a30789e7b03ec
Shivang Gupta <shivangguptaxd@gmail.com>
2022-12-03 00:42:29 +0530

    initial

---
+++ 1] Introduction/app.js
@@ -0,0 +1,18 @@
+// function getTotal(numbers) {
+//     return numbers.reduce((acc,item)=>{
+//         return acc+item;
+//     },0)
+// }
+// console.log(getTotal([3,2,1]));
+// function getTotal(numbers: number[]) {
+//     return numbers.reduce((acc,item)=>{
+//         return acc+item;
+//     },0)
+// }
+// console.log(getTotal([3,2,1]));
+function getTotal(numbers) {
+    return numbers.reduce(function (acc, item) {
+        return acc + item;
+    }, 0);
+}
+console.log(getTotal([3, 2, 1]));
